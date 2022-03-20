8f5558bae0f97ef5e087436a2bbdc4c769fd81eb
Archerd6 <archerd@uma.es>
2022-03-20 01:00:37 +0100

    1.0

--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -8,22 +8,43 @@ import io.cucumber.java.en.When;
 
 public class StepDefinitions
 {
-    
     private Calculadora_Archerd6 calculadora;
     private double Resultado;
+    private Boolean Resultado_booleano;
     
     @Given("I have a Calculator")
     public void i_have_a_calculator()
     {
-    // Write code here that turns the phrase above into concrete actions
+    	// Write code here that turns the phrase above into concrete actions
     	calculadora = new Calculadora_Archerd6();
     }
+    
     @When("I sum {int} and {int}")
-    public void i_sum_and(Integer int1, Integer int2)
+    public void i_sum_and(double int1, double int2)
     {
-    // Write code here that turns the phrase above into concrete actions
     	Resultado = calculadora.suma(int1, int2);
     }
+	@When("I substract {int} value {int}")
+	public void i_substract_value(double int1, double int2)
+	{
+		Resultado = calculadora.resta(int1, int2);
+	}
+	@When("I multiply {int} value {int}")
+	public void i_multiply_value(double int1, double int2)
+	{
+		Resultado = calculadora.mult(int1, int2);
+	}
+	@When("I divide {int} value {int}")
+	public void i_divide_value(Integer int1, Integer int2)
+	{
+		Resultado = calculadora.divide(int1, int2);
+	}
+	@When("I select the factorial of {int}")
+	public void i_select_the_factorial_of(Integer int1)
+	{
+		Resultado = calculadora.fact(int1);
+	}
+	
     @Then("the result is {double}")
     public void the_result_is(double Entrante)
     {
@@ -32,6 +53,17 @@ public class StepDefinitions
     }
     
     
+    @When("I ask if {int} is prime")
+    public void i_ask_if_is_prime(Integer int1)
+    {
+    	Resultado_booleano = calculadora.esPrimo(int1);
+    }
+    
+    @Then("the result boolean as int is {int}")
+    public void the_result_boolean_as_int_is(Integer int1)
+    {
+    	Assertions.assertEquals(int1, Resultado_booleano?1:0);
+    }
     
 
 
--- src/test/resources/io/cucumber/skeleton/Test.feature
+++ src/test/resources/io/cucumber/skeleton/Test.feature
@@ -1,14 +1,39 @@
 Feature: TestGlobal
 
-
-   Scenario Outline: SumaOk
+   Scenario Outline: Suma
     Given I have a Calculator
     When I sum <a> and <b>
     Then the result is <value>
 
     Examples: 
-         |  a   |  b  |  value  |
-         |  5   |  1  |    6    |
-         |  123   |  9  |    132    |
-         |  876  |  557  |    1433   |
-         |  2   |  5543  |    5545    |
+         | a |  b  |  value  |
+         | 5 |  1  |    6    |
+         |123|  9  |   132   |
+         |876| 557 |   1433  |
+         | 2 | 5543|   5545  |
+
+
+   Scenario: Resta
+    Given I have a Calculator
+    When I substract 88 value 8
+    Then the result is 80
+    
+   Scenario: Multiplicacion
+    Given I have a Calculator
+    When I multiply 88 value 8
+    Then the result is 704
+    
+   Scenario: Division
+    Given I have a Calculator
+    When I divide 88 value 8
+    Then the result is 11
+   
+   Scenario: Factorial
+    Given I have a Calculator
+    When I select the factorial of 3
+    Then the result is 6
+    
+   Scenario: Primo
+    Given I have a Calculator
+    When I ask if 7542 is prime
+    Then the result boolean as int is 0
